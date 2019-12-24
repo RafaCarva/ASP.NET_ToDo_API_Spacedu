e99547c83beddc76684c12e83b15a5a8e98fd813
Rafael Faria Carvalho <rafael.carvalho@programmers.com.br>
2019-12-24 15:58:27 -0300

    Criado o BD

---
+++ API/Database/MinhasTarefasContext.cs
@@ -0,0 +1,20 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Database
+{
+    public class MinhasTarefasContext : IdentityDbContext<ApplicationUser>
+    {
+        public MinhasTarefasContext(DbContextOptions<MinhasTarefasContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -18,13 +20,16 @@ namespace API
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-        }
+        } 
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MinhasTarefasContext>(op => {
+                op.UseSqlite("Data Source=Database\\MinhasTarefas.db");
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
